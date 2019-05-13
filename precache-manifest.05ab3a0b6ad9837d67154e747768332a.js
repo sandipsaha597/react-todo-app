43073dff38ff794c3f4b230ad9f90b8183a0e6e2
sandipsaha597 <40121135+sandipsaha597@users.noreply.github.com>
2019-05-13 14:00:49 +0530

    Add files via upload

---
+++ precache-manifest.05ab3a0b6ad9837d67154e747768332a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8277e3bfd5ad473f21f0e2a514e70860",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "a0f535824285d33e3d00",
+    "url": "/react-todo-app/static/css/main.6daf4490.chunk.css"
+  },
+  {
+    "revision": "a6ea6394b56e4f7d16dc",
+    "url": "/react-todo-app/static/js/2.cb5cb5a1.chunk.js"
+  },
+  {
+    "revision": "a0f535824285d33e3d00",
+    "url": "/react-todo-app/static/js/main.535d20b4.chunk.js"
+  },
+  {
+    "revision": "b1aeed9f6344163e5e40",
+    "url": "/react-todo-app/static/js/runtime~main.a3a648b2.js"
+  }
+]);
